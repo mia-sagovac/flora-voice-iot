334a8f973f69ba011ae9fa5cde88f686a7b86736
mia-sagovac <mia.sagovac@fer.hr>
2026-05-27 10:59:26 +0200

    dodan cors za login preko fronta

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,6 +6,7 @@ fastapi dev app/main.py
 """
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.config import settings
 from app.routers import test
@@ -15,6 +16,14 @@ app = FastAPI(
     title="FastAPI + ThingsBoard",
     description="Demo: autentikacija preko ThingsBoarda + zaštićeni endpoint",
     version="0.1.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Registriraj routere
