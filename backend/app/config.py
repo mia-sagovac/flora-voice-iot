67b540bfb5ae932f257f8649ed765b0111d61c55
Jakov Lovaković <lovakovic.jakov30@gmail.com>
2026-06-03 17:13:26 +0200

    Fix formating for config.py
    Updated comments for thingsboard_username and thingsboard_password to clarify their usage.

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,8 +4,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     thingsboard_url: str = "http://161.53.133.253:8080"
-    thingsboard_username: str = ""  # iz .env
-    thingsboard_password: str = ""  # iz .env
+    thingsboard_username: str = "" # iz .env, ali vjerojatno nam nece trebat, vidjet cemo
+    thingsboard_password: str = "" # iz .env
     request_timeout: float = 10.0
 
     model_config = SettingsConfigDict(env_file=".env", case_sensitive=False)
